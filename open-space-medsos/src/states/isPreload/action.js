7ac3736cf40b7d6b2c88eaac00988ce339285347
Yahya Abdurrozaq <v.yahya.abdurrozaq@adira.co.id>
2023-01-09 21:28:31 +0700

    init open space projects

---
+++ open-space-medsos/src/states/isPreload/action.js
@@ -0,0 +1,40 @@
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import { setAuthUserActionCreator } from '../authUser/action';
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+    try {
+      // preload process
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // fallback process
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // end preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+    dispatch(hideLoading());
+  };
+}
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
